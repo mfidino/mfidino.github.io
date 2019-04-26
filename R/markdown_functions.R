2e179d3aa09a4a41a4790d8529d12a9671a9e7a1
Mason Fidino <mfidino@lpzoo.org>
2019-04-26 11:41:21 -0500

    typo on reserach page

--- R/markdown_functions.R
+++ R/markdown_functions.R
@@ -7,14 +7,14 @@ paste0("title: Time report for ",as.character(x$next_meeting),"\n"),
 paste0("---\n\n"),
 paste0("This is a summary of what I have done between ", as.character(x$time_range$start)," and " ,as.character(x$time_range$end),".\n"),
 paste0("At the most broad scale, here is the proportion of time I have spent working across four different categories: for UWIN, for those in UWI, for those in the Conservation & Science department, and external collaborations.\n"),
-paste0("<img src='{{site.baseurl}}/images/",x$next_meeting,"_category_plot.jpg' alt='The amount of time I spent across various categories' width='650' height='650'>\n"),
+paste0("<img src='{{site.baseurl}}/images/",x$next_meeting,"_category_plot.jpg' alt='The amount of time I spent across various categories' width='600' height='600'>\n"),
 paste0("Within each of these categories, I have worked with ", as.character(nrow(x$client))," clients. Split across these clients my last two weeks looked like:\n"),
-paste0("<img src='{{site.baseurl}}/images/",x$next_meeting,"_client_plot.jpg' alt='Who I worked with over the last two weeks' width='650' height='650'>\n"),
+paste0("<img src='{{site.baseurl}}/images/",x$next_meeting,"_client_plot.jpg' alt='Who I worked with over the last two weeks' width='600' height='600'>\n"),
 paste0("I worked on ", paste0(nrow(x$project))," projects. My time split amongst them was:\n"),
-paste0("<img src='{{site.baseurl}}/images/",x$next_meeting,"_project_plot.jpg' alt='The different projects I worked on' width='650' height='650'>\n"),
+paste0("<img src='{{site.baseurl}}/images/",x$next_meeting,"_project_plot.jpg' alt='The different projects I worked on' width='600' height='600'>\n"),
 paste0("Of the projects that I work on there are ",nrow(x$weeks_since), " that I believe will result in publication.\n"),
 paste0("Here is the last I have worked on each of these projects:\n"),
-paste0("<img src='{{site.baseurl}}/images/",x$next_meeting,"_weeks_since.jpg' alt='How long has it been since I visited different projects' width='650' height='650'>\n"),
+paste0("<img src='{{site.baseurl}}/images/",x$next_meeting,"_weeks_since.jpg' alt='How long has it been since I visited different projects' width='600' height='600'>\n"),
 fill = TRUE, sep = "")
   sink()
 }
